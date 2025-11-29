351eb307ddd558efd48ca59f00675739e0b451b3
Takumi Akimoto <81888693+akimon658@users.noreply.github.com>
2025-11-29 14:43:45 +0900

    Add default password

--- db/index.ts
+++ db/index.ts
@@ -7,7 +7,7 @@ const getConnectionConfig = () => {
     host: Deno.env.get("DB_HOST") || "localhost",
     port: parseInt(Deno.env.get("DB_PORT") || "3306"),
     user: Deno.env.get("DB_USER") || "root",
-    password: Deno.env.get("DB_PASSWORD") || "",
+    password: Deno.env.get("DB_PASSWORD") || "password",
     database: Deno.env.get("DB_NAME") || "duety",
   };
 };
