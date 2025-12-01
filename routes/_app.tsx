eb51d3f65d24124039c9e9be351fbb34c779825b
Takumi Akimoto <81888693+akimon658@users.noreply.github.com>
2025-12-01 16:40:49 +0900

    Add header

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -1,14 +1,32 @@
 import { define } from "@/lib/define.ts"
 
-export default define.page(({ Component }) => {
+export default define.page(({ Component, state }) => {
   return (
-    <html>
+    <html lang="ja">
       <head>
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>Duety</title>
       </head>
+
       <body>
+        <header className="max-w-2xl mx-auto navbar">
+          <div className="flex-1 text-xl">Duety</div>
+
+          <div className="flex gap-2 items-center">
+            <div>{state.user.username}</div>
+
+            <div className="avatar rounded-full w-8">
+              <img
+                alt="Avatar"
+                height={32}
+                src={`http://image-proxy.trap.jp/icon/${state.user.username}?width=128`}
+                width={32}
+              />
+            </div>
+          </div>
+        </header>
+
         <Component />
       </body>
     </html>
